5cb511f5c3207f8edc994d6d5b9a627d58eaf4df
oscar novoa <novthel@live.com>
2022-12-04 19:45:05 -0500

    Desarrollo: ajuste final deploy. on

---
+++ build/firebase-messaging-sw.js
@@ -0,0 +1,37 @@
+// // eslint-disable-next-line no-undef
+// importScripts("https://www.gstatic.com/firebasejs/9.13.0/firebase-app-compat.js")
+// // eslint-disable-next-line no-undef
+// importScripts("https://www.gstatic.com/firebasejs/9.13.0/firebase-messaging-compat.js")
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAXk-dpDDJJzAxxwwM0ComTP69rzgHFY1w",
+//   authDomain: "panaderia-web.firebaseapp.com",
+//   projectId: "panaderia-web",
+//   storageBucket: "panaderia-web.appspot.com",
+//   messagingSenderId: "711454574136",
+//   appId:"1:711454574136:web:a48e966f8b31b467fe48cd"
+// };
+
+
+// // Initialize Firebase
+// // eslint-disable-next-line no-undef
+// const app = firebase.initializeApp(firebaseConfig);
+// // eslint-disable-next-line no-undef
+// const messaging = firebase.messaging(app);
+
+// messaging.onBackgroundMessage(payload => {
+//   console.log('mensaje recibido..');
+
+//   const notificationTitle = 'Titulo panaderia';
+//   const notificationOptions = {
+//     body: 'Promocion de panes',
+//     icon: './logo-panaderia.png'
+//   }
+
+//   // eslint-disable-next-line no-restricted-globals
+//   return self.registration.showNotification( notificationTitle, notificationOptions);
+
+// })
+
+
